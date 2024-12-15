744f8e90a2260e7f777b3aceaeeb28abdd752c84
IlyaPopovLa <il.popov@lamoda.ru>
2024-12-15 09:35:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 password = input("Введите пароль: ")
 
+
 if len(password) <= 12:
     print('Короткий')
 else:
@@ -14,8 +15,16 @@ for letter in password:
         print(letter, 'Буква')
 
 
-found_digit = 'Нет цифр'
-for digit in password:
-    if digit.isdigit():
-        found_digit = 'Есть цифры'
-print(found_digit)
+# found_digit = 'Нет цифр'
+# for digit in password:
+    # if digit.isdigit():
+        # found_digit = 'Есть цифры'
+# print(found_digit)
+
+def has_digit(password):
+    found_digit = 'Нет цифр'
+    for digit in password:
+        if digit.isdigit():
+            found_digit = 'Есть цифры'
+    return found_digit
+print(has_digit)
